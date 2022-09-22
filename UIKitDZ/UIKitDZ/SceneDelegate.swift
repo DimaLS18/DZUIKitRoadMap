bc229f93fc51f21416e5bf76db5249b70c94185a
DimaLS18 <kovrigindima@gmail.com>
2022-09-22 19:41:11 +0300

    Шаблон для UIKit

---
+++ UIKitDZ/UIKitDZ/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  UIKitDZ
+//
+//  Created by Dima Kovrigin on 22.09.2022.
+//
+
+import UIKit
+/// Начальный делегат
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Dima Kovrigin on 22.09.2022.
+//
+
+import UIKit
+/// начальный VC
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
