39827afc3734ef4e2d7d97545756236a4a9eea2b
DimaLS18 <kovrigindima@gmail.com>
2022-10-06 16:09:28 +0300

    выполнил 15 дз

---
+++ UIKitDZ/UIKitDZ/MainViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Dima Kovrigin on 22.09.2022.
+//
+
+import UIKit
+/// начальный VC
+class MainViewController: UIViewController {
+
+    // MARK: - Constants
+    private enum Constants {
+        static let timerText = "секунд"
+        static let defaultTimerValue: Float = 0.0
+        static let startButtonTitle = "Старт"
+        static let pauseButtonTitle = "Пауза"
+    }
+
+    // MARK: - Private IBOutlets
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var activateTimerButton: UIButton!
+
+    // MARK: - Private properties
+    private var timer: Timer?
+    private var isTimerActive = false {
+        didSet {
+            setupTimerLogic(by: isTimerActive)
+        }
+    }
+    private var currentTimerTime = Constants.defaultTimerValue {
+        didSet {
+            updateTimerLabel(with: currentTimerTime)
+        }
+    }
+
+    // MARK: - Private Methods
+
+    private func setupTimerLogic(by isTimerActive: Bool) {
+        guard isTimerActive else {
+            setupNonActiveTimer()
+            return
+        }
+        setupActiveTimer()
+    }
+
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,
+                                     selector: #selector(updateTimerValue), userInfo: nil, repeats: true)
+    }
+
+    private func pauseTimer() {
+        timer?.invalidate()
+    }
+
+    private func updateTimerLabel(with time: Float) {
+        let time = String(format: "%.1f", currentTimerTime)
+        timerLabel.text = "\(time) \(Constants.timerText)"
+    }
+
+    private func setupNonActiveTimer() {
+        pauseTimer()
+        activateTimerButton.setTitle(Constants.startButtonTitle, for: .normal)
+        activateTimerButton.backgroundColor = .systemGreen
+    }
+
+    private func setupActiveTimer() {
+        startTimer()
+        activateTimerButton.setTitle(Constants.pauseButtonTitle, for: .normal)
+        activateTimerButton.backgroundColor = .systemOrange
+    }
+
+    private func setupStoppedTimer() {
+        currentTimerTime = Constants.defaultTimerValue
+    }
+
+    @objc private func updateTimerValue(_ sender: Timer) {
+        currentTimerTime += Float(sender.timeInterval)
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func activateTimerButtonAction(_ sender: UIButton) {
+        isTimerActive.toggle()
+    }
+
+//    @IBAction private func activateTimerButtonAction(_ sender: UIButton) {
+//        isTimerActive.toggle()
+//    }
+
+    @IBAction func stopTimerButtonAction(_ sender: Any) {
+        setupStoppedTimer()
+        setupNonActiveTimer()
+    }
+    //    @IBAction func stopTimerButtonAction(_ sender: UIButton) {
+//        setupStoppedTimer()
+//        setupNonActiveTimer()
+//    }
+}
