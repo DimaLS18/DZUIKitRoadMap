8f877f35ad13395b6ecb9b5d0e4168fb221ff6a3
DimaLS18 <kovrigindima@gmail.com>
2022-10-06 09:09:03 +0300

    исправил замечания

--- UIKitDZ/UIKitDZ/MainViewController.swift
+++ UIKitDZ/UIKitDZ/MainViewController.swift
@@ -24,30 +24,37 @@ class MainViewController: UIViewController {
     }
 
     @IBAction func shareButtonTapped(_ sender: Any) {
-        guard let grade = gradeSegment.titleForSegment(at: gradeSegment.selectedSegmentIndex),
-         let name = nameTextfield.text,
-         let image = UIImage(named: "olimp") else { return }
-         let text = "Секция: " + grade + " Name: " + name
-         let activityController = UIActivityViewController(activityItems: [image, text], applicationActivities: nil)
-         present(activityController, animated: true)
-     }
- }
+        guard
+            let grade = gradeSegment.titleForSegment(at: gradeSegment.selectedSegmentIndex),
+            let name = nameTextfield.text,
+            let image = UIImage(named: "olimp")
+        else {
+            return
+
+        }
+        let text = "Секция: " + grade + " Name: " + name
+        let activityController = UIActivityViewController(activityItems: [image, text], applicationActivities: nil)
+        present(activityController, animated: true)
+    }
+}
+
+/// UIPickerViewDataSource, UIPickerViewDelegate
 extension MainViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 
-     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-         1
-     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
 
-     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-         pickerData.count
-     }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData.count
+    }
 
-     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-         pickerData[row]
-     }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        pickerData[row]
+    }
 
-     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-         guard row == 2 else { return }
-         shareButtonTapped(self)
-     }
- }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        guard row == 2 else { return }
+        shareButtonTapped(self)
+    }
+}
