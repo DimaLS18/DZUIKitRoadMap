6f746dc09377da7a42f1ed363d2ec7bd7e78384c
DimaLS18 <kovrigindima@gmail.com>
2022-10-06 13:53:04 +0300

    выполнил 12 ДЗ

---
+++ UIKitDZ/UIKitDZ/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  UIKitDZ
+//
+//  Created by Dima Kovrigin on 06.10.2022.
+//
+
+import UIKit
+/// второй контроллер
+final class SecondViewController: UIViewController {
+ // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.title = "Second VC"
+
+    }
+}
